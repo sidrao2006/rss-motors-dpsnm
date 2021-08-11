c594363b42e5d9877198b646e3791b7cb4a62bc1
Bhavya Maheshwari <you@example.com>
2021-08-11 18:30:59 +0530

    TopBar made.

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,10 +1,16 @@
-import './App.css';
 import { Component } from 'react';
+
+// importing components
+import Topbar from './components/Topbar';
+
+// importing bootstrap stylings
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="Dashboard">
+      <div className="Dashboard"> 
+      <Topbar/>
       </div>
     );
   }
