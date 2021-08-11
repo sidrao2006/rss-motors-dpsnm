cf0c2cf3edbb434bb8412e474d82391b63773995
Aneesh Rao <sidrao2006@gmail.com>
2021-08-11 15:04:02 +0530

    update sources for bootstrap

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,10 +1,14 @@
-import './App.css';
 import { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+import './styles/dashboard.css';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="Dashboard">
+        <Container fluid>
+        </Container>
       </div>
     );
   }
