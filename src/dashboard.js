ee9a93fef436e52f8f3bfbbb549d1ba9e9128535
Aneesh Rao <sidrao2006@gmail.com>
2021-08-11 11:16:19 +0530

    refactor: Remove unnecessary boilerplate

---
+++ src/dashboard.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
