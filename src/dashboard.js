4015a255d1711a9426ede667bceda73f59cc190a
Aneesh Rao <sidrao2006@gmail.com>
2021-08-11 15:21:15 +0530

    Add comments and layout modifiers

--- src/dashboard.js
+++ src/dashboard.js
@@ -6,8 +6,40 @@ import './styles/dashboard.css';
 class Dashboard extends Component {
   render() {
     return (
-      <div className="Dashboard">
+      <div className="Dashboard"
+        style={{
+          background: 'linear-gradient(to bottom, #ffffff 0%, #000000 100%)',
+        }}
+      >
         <Container fluid>
+
+          <Row>
+            {/* Top bar */}
+          </Row>
+
+          <Row>
+            <Col>
+
+              <Row>
+                {/* Map */}
+              </Row>
+
+              <Row>
+                {/* Sound cloud audio player */}
+              </Row>
+
+            </Col>
+
+            <Col>
+              {/* Button panel */}
+            </Col>
+
+          </Row>
+
+          <Row>
+            {/* Bottom bar */}
+          </Row>
+
         </Container>
       </div>
     );
