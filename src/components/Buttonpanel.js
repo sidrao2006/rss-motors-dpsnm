52fafff067ff44fa99ae5c449db93d6d62c43ee0
Bhavya Maheshwari <you@example.com>
2021-08-12 18:33:08 +0530

    modified into button Buttonpanel

--- src/components/Buttonpanel.js
+++ src/components/Buttonpanel.js
@@ -3,16 +3,16 @@ import '../components/Buttonpanel.css'
 export default function Buttonpanel() {
   return (
     <>
-      <div className="container d-flex flex-wrap justify-content-around align-items-center  m-3" >
+      <button className=" panelItems container d-flex flex-wrap justify-content-around align-items-center  m-3" >
         <div className="panelButtons d-flex justify-content-around align-items-center shadow" >
           <i className="fas fa-parking d-inline-block dashboard-text" style={{ fontSize: "30px" }}></i>
         </div>
         <div className="panelButtons d-flex justify-content-around align-items-center shadow" >
           <i className="fas fa-location-arrow d-inline-block dashboard-text" style={{ fontSize: "30px" }}></i>
         </div>
-      </div>
+      </button>
 
-      <div className="container d-flex flex-wrap justify-content-around align-items-center  m-3" >
+      <button className="panelItems container d-flex flex-wrap justify-content-around align-items-center  m-3" >
         <div className="panelButtons d-flex justify-content-around align-items-center shadow">
           <i className="fa fa-phone dashboard-text" style={{ fontSize: "30px", transform: "rotateY(180deg)" }}></i>
         </div>
@@ -20,9 +20,9 @@ export default function Buttonpanel() {
         <div className="panelButtons d-flex justify-content-around align-items-center shadow" >
           <i className="fas fa-video dashboard-text" style={{ fontSize: "30px" }}></i>
         </div>
+      </button>
 
-      </div>
-      <div className="container d-flex flex-wrap justify-content-around align-items-center m-3" >
+      <button className="panelItems container d-flex flex-wrap justify-content-around align-items-center m-3" >
         <div className="panelButtons d-flex justify-content-around align-items-center shadow" >
           <i className="fas fa-angle-left d-inline-block dashboard-text" style={{ fontSize: "40px" }}></i>
 
@@ -30,9 +30,9 @@ export default function Buttonpanel() {
         <div className="panelButtons d-flex justify-content-around align-items-center shadow" >
           <i className="fas fa-angle-right dashboard-text" style={{ fontSize: "40px" }}></i>
         </div>
-      </div>
+      </button>
 
-      <div className="container d-flex flex-wrap justify-content-around align-items-center m-3" >
+      <button className="panelItems container d-flex flex-wrap justify-content-around align-items-center m-3" >
         <button className="button d-flex justify-content-around align-items-center shadow" style={{ width: "140px", height: "50px", borderRadius: "10px", }}>
           <i className="fas fa-address-card d-inline-block dashboard-text" style={{ fontSize: "30px", cursor: "pointer" }}></i><span className="dashboard-text" style={{ fontWeight: "600", cursor: "pointer" }}>Contacts</span>
 
@@ -42,7 +42,7 @@ export default function Buttonpanel() {
           <i className="fas fa-sun dashboard-text" style={{ fontSize: "20px" }}></i>
 
         </button>
-      </div>
+      </button>
 
     </>
 
